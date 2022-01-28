78dabd98af39dce825db94036e597cf24a4459e7
MagnusdeAart <magnus.surmann@web.de>
2022-01-28 15:33:49 +0200

    added rania functionality

--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
 	print("Hello")
 
+def rania():
+	print("grrrrrr, i am a tiger!")
+
 def main():
-	default()
+	if sys.argv[1] == 'rania':
+		rania()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
